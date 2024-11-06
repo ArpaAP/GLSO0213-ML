35505b0fcc068ddd061ea3faa14dff054f39c7af
Buyeon Hwang <arpa1705@gmail.com>
2024-11-07 08:34:44 +0900

    Update random_sample.py

--- random_sample.py
+++ random_sample.py
@@ -1,8 +1,13 @@
 import os
 import shutil
 import random
+from tqdm import tqdm
 
 def copy_random_files(src_folder, dest_folder, num_files):
+    # 대상 폴더가 존재하지 않으면 생성합니다.
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+
     # 소스 폴더에 있는 모든 파일 목록을 가져옵니다.
     all_files = os.listdir(src_folder)
 
@@ -15,12 +20,11 @@ def copy_random_files(src_folder, dest_folder, num_files):
     selected_files = random.sample(all_files, num_files)
     
     # 선택된 파일을 대상 폴더로 복사합니다.
-    for file_name in selected_files:
+    for file_name in tqdm(selected_files):
         src_path = os.path.join(src_folder, file_name)
         dest_path = os.path.join(dest_folder, file_name)
         shutil.copy(src_path, dest_path)
-        print(f"Copied: {file_name} to {dest_folder}")
 
-src_folder = 'D:\\Datasets\\BirdsFlatten'  # 소스 폴더 경로
-dest_folder = 'D:\\Datasets\\BirdsRandom'  # 대상 폴더 경로
-copy_random_files(dest_folder, dest_folder, 10000)
+src_folder = r'D:\\Datasets\\CarsFlatten'  # 소스 폴더 경로
+dest_folder = r'D:\\Datasets\\CarsFlattenRandom'  # 대상 폴더 경로
+copy_random_files(src_folder, dest_folder, 20000)
