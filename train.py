1e459ff8fcddce2711a9b068030bc22fd790dc1f
Buyeon Hwang <arpa1705@gmail.com>
2024-11-06 22:50:15 +0900

    feat: 모델 크기 증가

--- train.py
+++ train.py
@@ -52,22 +52,27 @@ class CNNClassifier(nn.Module):
         self.bn2 = nn.BatchNorm2d(128)
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(256)
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(512)
         self.pool = nn.MaxPool2d(2, 2)
         self.dropout = nn.Dropout(p=0.1)
-        self.fc1 = nn.Linear(256 * 16 * 16, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 5)
+        self.fc1 = nn.Linear(512 * 8 * 8, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 5)
 
     def forward(self, x):
         x = self.pool(F.relu(self.bn1(self.conv1(x))))
         x = self.pool(F.relu(self.bn2(self.conv2(x))))
         x = self.pool(F.relu(self.bn3(self.conv3(x))))
-        x = x.view(-1, 256 * 16 * 16)
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+        x = x.view(-1, 512 * 8 * 8)
         x = self.dropout(x)
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
 
         return x
 
